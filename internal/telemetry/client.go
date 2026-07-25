d2e72119601798d6cd5e9debe64c36beb58eb560
mithulpranav24 <mithulpranav24826@gmail.com>
2026-07-26 01:49:27 +0530

    Scaffold Watchdog Agent backend structure

---
+++ internal/telemetry/client.go
@@ -0,0 +1,11 @@
+package telemetry
+
+// Client handles communication with the Prometheus API to fetch resource usage metrics.
+type Client struct {
+	// TODO: Add the Prometheus API connection client.
+}
+
+// NewClient initializes a new Telemetry client.
+func NewClient() *Client {
+	return &Client{}
+}
